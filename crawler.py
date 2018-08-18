168ae75c80e4f6b07f85683cb344f8b33d012320
Tommy Chiang <oToToT@users.noreply.github.com>
2018-08-19 01:20:44 +0800

    Update Crawler For New TIOJ

--- crawler.py
+++ crawler.py
@@ -7,8 +7,8 @@ def eprint(*args, **kwargs):
 
 def getResult(a):
     a=a.lower()
-    req = requests.get("http://tioj.infor.org/users/"+a.lower(), allow_redirects=False)
-    if req.headers['Status'] != "200 OK":
+    req = requests.get("https://tioj.ck.tp.edu.tw/users/"+a.lower(), allow_redirects=False)
+    if req.status_code != 200:
         return (False, [])
     else:
         sp = BeautifulSoup(req.text, "html.parser")
@@ -17,7 +17,7 @@ def getResult(a):
 
 def getPage():
     eprint("Getting Page")
-    req = requests.get("http://tioj.infor.org/users", allow_redirects=False)
+    req = requests.get("https://tioj.ck.tp.edu.tw/users", allow_redirects=False)
     sp = BeautifulSoup(req.text, "html.parser")
     return int(sp.find("div", class_="pagination").findAll("a")[-2].text)
 
@@ -26,21 +26,21 @@ def getNext(a):
     a=a.lower()
     for i in range(1,getPage()+1):
         eprint("Getting Page %d"%i)
-        req = requests.get(f"http://tioj.infor.org/users?page={i}")
+        req = requests.get(f"https://tioj.ck.tp.edu.tw/users?page={i}")
         found = BeautifulSoup(req.text, "html.parser").find("a", {"href": ("/users/"+a)})
         if found:
             eprint("Found in Page %d"%i)
             nxt = found.parent.parent.previous_sibling.previous_sibling
             if nxt:
-                return nxt.find("a")['href'].split("/")[2] 
+                return nxt.find("a")['href'].split("/")[2]
             else:
                 if i==1:
                     return False
                 else:
-                    temp = BeautifulSoup( requests.get("http://tioj.infor.org/users?page="+str(i-1)).text, "html.parser").findAll("a", href=re.compile(r'/users/[^\?]'))
+                    temp = BeautifulSoup( requests.get("httsp://tioj.ck.tp.edu.tw/users?page="+str(i-1)).text, "html.parser").findAll("a", href=re.compile(r'/users/[^\?]'))
                     return temp[len(temp)-1].text
     return False
-        
+
 
 def Compare(a, b):
     if not a or not b:
@@ -68,3 +68,4 @@ if __name__ == "__main__":
         print(json.dumps(res))
     else:
         print('{"status":500,"error": "error occured"}')
+
